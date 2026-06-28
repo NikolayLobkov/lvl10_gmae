85226c0ca5784f8fdadb078039709a8b4ae30eb8
nit <49253280+nitnatfiftifant@users.noreply.github.com>
2026-06-29 00:07:12 +0500

    shader

---
+++ game/ui/settings_menu/settings_menu.gd
@@ -0,0 +1,11 @@
+extends Control
+class_name SettingMenu
+
+@onready var sensitivity_mouse_slider: HSlider = %SensitivityMouseSlider
+@onready var resolution_option_button: OptionButton = %ResolutionOptionButton
+@onready var window_mode_option_button: OptionButton = %WindowModeOptionButton
+@onready var music_slider: HSlider = %MusicSlider
+@onready var sfx_slider: HSlider = %SFXSlider
+
+@onready var apply_button: Button = %ApplyButton
+@onready var back_button: Button = %BackButton

cdc01da31a09bdfcb2f32384d834478b0178057e
nit <49253280+nitnatfiftifant@users.noreply.github.com>
2026-06-28 15:19:10 +0500

    test fps

--- game/gameplay/systems/C_look.gd
+++ game/gameplay/systems/C_look.gd
@@ -12,4 +12,4 @@ func _ready() -> void:
 func _input_tick(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		actor.rotation.y -= event.relative.x * 0.005
-		pivot.rotation.x = clampf(pivot.rotation.x - event.relative.y * 0.005, -PI / 2.0, PI / 2.0)
+		pivot.rotation.x = clampf(pivot.rotation.x - event.relative.y * 0.005, -0.8, PI / 2.0)

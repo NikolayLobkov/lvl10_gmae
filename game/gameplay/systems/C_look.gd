1c91bdcccb5814830ecf85950b742b2361a15bcd
Nikolay Lobkov <nikolaylobkov1272@gmail.com>
2026-06-28 13:22:10 +0300

    Update C_look.gd

--- game/gameplay/systems/C_look.gd
+++ game/gameplay/systems/C_look.gd
@@ -2,6 +2,10 @@
 class_name LookComponent extends Node
 
 
+const MIN_ANGLE: float = -PI / 3.0
+const MAX_ANGLE: float = PI / 2.0
+
+
 @export var actor: Node3D
 @export var pivot: Node3D
 
@@ -12,4 +16,4 @@ func _ready() -> void:
 func _input_tick(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		actor.rotation.y -= event.relative.x * 0.005
-		pivot.rotation.x = clampf(pivot.rotation.x - event.relative.y * 0.005, -0.8, PI / 2.0)
+		pivot.rotation.x = clampf(pivot.rotation.x - event.relative.y * 0.005, MIN_ANGLE, MAX_ANGLE)

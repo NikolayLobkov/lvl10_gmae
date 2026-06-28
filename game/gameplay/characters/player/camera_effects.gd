dc10b16c16272c29c0148569b7dcefacc584c276
Nikolay Lobkov <nikolaylobkov1272@gmail.com>
2026-06-28 15:33:04 +0300

    фаывывипяывыврявкрт

---
+++ game/gameplay/characters/player/camera_effects.gd
@@ -0,0 +1,8 @@
+extends Node
+
+
+
+
+
+func _tick(_delta: float) -> void:
+	pass
--- game/gameplay/systems/C_health.gd
+++ game/gameplay/systems/C_health.gd
@@ -5,19 +5,27 @@ class_name HealthComponent extends Node
 signal health_changed(value: float)
 signal max_health_changed(value: float)
 signal damage_taked(data: AttackData)
+signal death
 
-
-
-var health: float = 100.0: set = change_health
 @export_range(1.0, 1000.0, 1.0, 'or_greater') var max_health: float = 100.0: set = change_max_health
 
+var health: float = 100.0: set = change_health
+var dead: bool = false
 
 
 func change_health(value: float) -> void:
-	health = clampf(value, 0.0, max_health)
+	value = clampf(value, 0.0, max_health)
+	health = value
 	health_changed.emit(value)
+	
+	if value <= 0.0 and not dead:
+		dead = true
+		death.emit()
+	else:
+		dead = false
 func change_max_health(value: float) -> void:
-	max_health = maxf(max_health, 1.0)
+	value = maxf(value, 1.0)
+	max_health = value
 	max_health_changed.emit(value)
 func damage(data: AttackData) -> void:
 	if data:
